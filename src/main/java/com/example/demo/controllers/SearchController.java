636b375a144a04cc546a6250f32677b2106fb22c
Eerik Anttila <eerik.anttila@siili.com>
2024-04-28 15:14:22 +0300

    Connection test

--- src/main/java/com/example/demo/controllers/SearchController.java
+++ src/main/java/com/example/demo/controllers/SearchController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -34,5 +35,32 @@ public class SearchController {
                 .retrieve()
                 .bodyToMono(String.class)
                 .map(response -> ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(response));
+    }
+
+    @GetMapping("/checkConnection")
+    public Mono<ResponseEntity<String>> checkConnection() {
+        WebClient client = WebClient.create();
+        final String uri = "https://fineli.fi/fineli/api/v1/foods?q=kana";
+
+        return client.get()
+                .uri(uri)
+                .retrieve()
+                .toBodilessEntity()
+                .flatMap(response -> {
+                    if (response.getStatusCode().is2xxSuccessful()) {
+                        System.out.println("Connection to fineli.fi is successful");
+                        return Mono.just(ResponseEntity.ok().body("Connection to fineli.fi is successful"));
+                    } else {
+                        System.out.println("Connection to fineli.fi failed");
+                        return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                .body("Connection to fineli.fi failed"));
+                    }
+                })
+                .onErrorResume(error -> {
+                    // Handle the error here
+                    System.out.println("Connection to fineli.fi failed");
+                    return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                            .body("Connection to fineli.fi failed"));
+                });
     }
 }
