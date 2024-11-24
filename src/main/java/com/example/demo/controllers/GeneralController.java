0f3fe392db9b3e4abac15017220d128f4a59d02d
eerikanttilasiili <eerik.anttila@siili.com>
2024-11-24 15:48:55 +0200

    Packaged

--- src/main/java/com/example/demo/controllers/GeneralController.java
+++ src/main/java/com/example/demo/controllers/GeneralController.java
@@ -4,19 +4,14 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.jsonwebtoken.io.IOException;
 
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
